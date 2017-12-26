f18adeda442dc2a51f501047487e0d8c062d8126
Daniel Bajana <it.danielbajana@gmail.com>
2017-12-26 10:13:09 +0800

    Change app name to NEWSIM MObile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NEWSIM App</title>
+        <title>NEWSIM Mobile</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NEWSIM APP
+                    NEWSIM MOBILE
                 </div>
             </div>
         </div>
