1cf5dd8a235efd70d98792afea94126afc4480ad
Esmeraldo Barrios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-01-07 10:44:12 +0800

    Change Maximum Post Image to 10

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -9,9 +9,9 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         // Create 23 Post with minimum number of Images of 0
-        // and maximum number of 0 for each Post created.
+        // and maximum number of 10 for each Post created.
         factory(Post::class, 23)->create()->each(function ($post) {
-            for ($i=0; $i < rand(0, 0); $i++) {
+            for ($i=0; $i < rand(0, 10); $i++) {
                 $post->postImages()->save(factory(PostImage::class)->make());
             }
         });
