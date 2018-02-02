2ec6eabf6cb2e90d97f43aa78ddcc23f40a56a91
Esmeraldo Barrios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-02-02 14:18:36 +0800

    Refactor to use covers on specified folder

--- database/seeds/OfficesTableSeeder.php
+++ database/seeds/OfficesTableSeeder.php
@@ -18,7 +18,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "3F LKC Bldg. Locsin-Burgos St. Brgy. 11 Bacolod City 6100",
             "telephone_no" => "(034)4350701",
             "location" => "geo:10.671450, 122.949501?q=10.671450, 122.949501(NEWSIM BACOLOD)&z=18",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_bacolod.jpg",
+            "photo_url" => "offices-cover/cover_bacolod.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Cebu",
@@ -26,7 +26,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "6F 731 Bldg. Escario St. Brgy. Capitol Site, Cebu City 6000",
             "telephone_no" => "(032)5203141 or 5203747",
             "location" => "geo:10.316989, 123.892501?q=NEWSIM+CEBU",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_cebu.jpg",
+            "photo_url" => "offices-cover/cover_cebu.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Cebu Annex Capitol Square",
@@ -34,7 +34,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "3F Capitol Square, Escario St. Brgy. Capitol Site, Cebu City 6000",
             "telephone_no" => "(032)5203141 or 5203747",
             "location" => "geo:10.3169110, 123.8938355?q=10.3169110, 123.8938355(NEWSIM CEBU Annex)&z=18",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_cebu_annex.jpg",
+            "photo_url" => "offices-cover/cover_cebu_annex.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Cebu Practical Site",
@@ -42,7 +42,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "Zone 2, Brgy. Biasong, Talisay City",
             "telephone_no" => "(032)5203141 or 5203747",
             "location" => "geo:10.236360, 123.829362?q=NEWSIM+CEBU+Practical+Site",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_talisay.jpg",
+            "photo_url" => "offices-cover/cover_talisay.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Davao",
@@ -50,7 +50,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "3F Court View Hotel, Pink Waltes Bldg. Quimpo Blvd. Matina Davao City 8000",
             "telephone_no" => "(082)2850224 or +63(912)1084078",
             "location" => "geo:7.057441, 125.600810?q=NEWSIM+DAVAO",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_davao.png",
+            "photo_url" => "offices-cover/cover_davao.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Ilo-ilo",
@@ -58,7 +58,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "2F 402 Arguelles Bldg. E. Lopez St. Jaro, Iloilo City 5000",
             "telephone_no" => "(033)3203776",
             "location" => "geo:10.721861, 122.558733?q=NEWSIM+ILOILO",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_iloilo.png",
+            "photo_url" => "offices-cover/cover_iloilo.png",
         ]);
         Office::create([
             "name" => "NEWSIM Makati Marconi",
@@ -66,7 +66,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "2323 NEWSIM Bldg. Marconi St. Brgy. San Isidro, Makati City 1234",
             "telephone_no" => "(02)8882764 or 664-8357",
             "location" => "geo:14.554677, 121.001053?q=NEWSIM+MARCONI",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_marconi.jpg",
+            "photo_url" => "offices-cover/cover_marconi.jpg",
         ]);
         Office::create([
             "name" => "NEWSIM Makati Edison",
@@ -74,7 +74,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "5F 2053 Bldg. Edison St. Brgy. San Isidro, Makati City 1234",
             "telephone_no" => "(02)8882764 or 664-8357",
             "location" => "geo:14.553635, 121.003397?q=New+Simulator+Center+Of+The+Phils.+Inc)",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_makati.png",
+            "photo_url" => "offices-cover/cover_makati.png",
         ]);
         Office::create([
             "name" => "NEWSIM TRAINING ACADEMY",
@@ -82,7 +82,7 @@ class OfficesTableSeeder extends Seeder
             "address"=> "Monte Vista Beach Resort Brgy. Bignay 2 Sariaya Quezon 4322",
             "telephone_no" => "(02)2451195 or 8884544",
             "location" => "geo:13.840336, 121.473373?q=Newsim+Training+Academy,+Quezon+Eco+Tourism+Road,+Sariaya,+Calabarzon",
-            "photo_url" => "http://104.238.96.209/~newsimtms/mobile/uploads/cover_nta.jpg",
+            "photo_url" => "offices-cover/cover_nta.jpg",
         ]);
     }
 }
